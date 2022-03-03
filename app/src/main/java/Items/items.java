a17ea0b266105a7c9137bbe6b8d36fa4baf8741b
AnkitShaw-creator <ankitshaw667@gmail.com>
2022-03-04 00:03:19 +0530

    fixed the issue with retrieval of data from firebase

---
+++ app/src/main/java/Items/items.java
@@ -0,0 +1,46 @@
+package Items;
+
+public class items {
+
+    String description, price, quantity, title;
+    public items(){}
+
+    public items(String description, String price, String quantity, String title){
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
