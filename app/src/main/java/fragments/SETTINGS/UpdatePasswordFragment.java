dde70cce401cae91bc08697c70e395bd486ccef4
AnkitShaw-creator <ankitshaw667@gmail.com>
2022-03-26 00:34:14 +0530

    password updating

--- app/src/main/java/fragments/SETTINGS/UpdatePasswordFragment.java
+++ app/src/main/java/fragments/SETTINGS/UpdatePasswordFragment.java
@@ -2,65 +2,101 @@ package fragments.SETTINGS;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mystore.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UpdatePasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UpdatePasswordFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG= "UpdatePasswordFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private AuthCredential userCred;
+    private EditText mEmail, mOldPassword, mNewPassword;
+    private MaterialButton mUpdate;
     public UpdatePasswordFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UpdatePasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UpdatePasswordFragment newInstance(String param1, String param2) {
-        UpdatePasswordFragment fragment = new UpdatePasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_password, container, false);
+        View v = inflater.inflate(R.layout.fragment_update_password, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        user  = mAuth.getCurrentUser();
+
+        mEmail = v.findViewById(R.id.password_email_et);
+        mOldPassword = v.findViewById(R.id.password_old_et);
+        mNewPassword = v.findViewById(R.id.password_new_et);
+        mUpdate = v.findViewById(R.id.updatePassword);
+        if(user != null) {
+            setUI();
+        }
+        return v;
+    }
+
+    private void setUI() {
+
+        if(user != null) {
+            String email = user.getEmail();
+            mEmail.setText(email);
+            String oldPassword = String.valueOf(mOldPassword.getText());
+            String newPassword = String.valueOf(mNewPassword.getText());
+
+            mUpdate.setOnClickListener(view -> {
+                if(!email.equals("") && !oldPassword.equals("")) {
+                    userCred = EmailAuthProvider.getCredential(email, oldPassword);
+                    user.reauthenticate(userCred).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()) {
+                                            mAuth.sendPasswordResetEmail(email);
+                                            Log.d(TAG, "onComplete: Password changed");
+                                            Toast.makeText(getContext(), "Password was updated", Toast.LENGTH_SHORT).show();
+                                        }
+                                        else{
+                                            Log.e(TAG, "onComplete: Password  was changed", task.getException());
+                                            Toast.makeText(getContext(), "Password was not updated", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+
+                            } else {
+                                Toast.makeText(getContext(), "Password was not updated", Toast.LENGTH_SHORT).show();
+                                Log.e(TAG, "onComplete: ", task.getException());
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        else{
+            Log.e(TAG, "setUI:  User not found" );
+        }
     }
 }
